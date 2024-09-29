a03551f05d38594cd42141d18c2abe723aa1b53b
Ichurri <santiagoiturrivargas04@gmail.com>
2024-09-29 19:04:08 -0400

    Create UltrasoniceSensor object

---
+++ actuator-sensor/UltrasonicSensor.h
@@ -0,0 +1,24 @@
+class UltrasonicSensor {
+  private:
+    int trigPin;
+    int echoPin;
+
+  public:
+    UltrasonicSensor(int trigPin, int echoPin) {
+      this->trigPin = trigPin;
+      this->echoPin = echoPin;
+      pinMode(trigPin, OUTPUT);
+      pinMode(echoPin, INPUT);
+    }
+
+    float getDistance() {
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+
+      float duration = pulseIn(echoPin, HIGH);
+      return duration * 0.034 / 2;
+    }
+};
