54703887287602b889fe7de3c4916b75066ea428
Camilo Padilla <camilo.padilla@ucb.edu.bo>
2024-10-01 18:31:10 -0400

    cambios de ip

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -4,45 +4,43 @@
 #include "TcpClient.h"
 
 // Configuración de la red Wi-Fi y del servidor
-const char *SSID = "LABO13";
-const char *PASSWORD = "catolica13";
+const char *SSID = "LABO17";
+const char *PASSWORD = "catolica17";
 const char *SERVER_IP = "192.168.79.82"; // IP del servidor Java
 const int SERVER_PORT = 12345;           // Puerto del servidor
 
 // Instanciación de las clases
 WiFiConnection wifiConnection(SSID, PASSWORD);
 TcpClient tcpClient(SERVER_IP, SERVER_PORT);
-UltrasonicSensor sensor(35, 34); // Pines para el sensor ultrasónico (Trig: 5, Echo: 18)
+UltrasonicSensor sensor(17, 16); // Pines para el sensor ultrasónico (Trig: 5, Echo: 18)
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Conexión a WiFi
-  //wifiConnection.connect();
+  wifiConnection.connect();
 
   // Conexión al servidor TCP
-  //if (!tcpClient.connect())
-  //{
-    //Serial.println("Could not connect to the server. Rebooting...");
-    //ESP.restart();
-  //}
+  if (!tcpClient.connect())
+  {
+    Serial.println("Could not connect to the server. Rebooting...");
+    ESP.restart();
+  }
 }
 
 void loop()
 {
   // Leer la distancia del sensor ultrasónico
   float distance = sensor.getDistance();
-  //Serial.println("Distance: " + String(distance) + " cm");
-  Serial.print("distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  Serial.println("Distance: " + String(distance) + " cm");
+
 
   // Enviar la distancia al servidor Java
-  //tcpClient.sendData(String(distance));
+  tcpClient.sendData(String(distance));
 
   // Verificar si hay comandos del servidor (en este caso, no se espera ninguno)
   // Puedes implementar lógica adicional si es necesario
 
-  //delay(1000); // Espera de 1 segundo antes de la siguiente lectura
+  delay(1000); // Espera de 1 segundo antes de la siguiente lectura
 }
